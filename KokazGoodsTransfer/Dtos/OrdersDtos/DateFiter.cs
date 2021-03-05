33cb5559f992eb7d539cfa1452cea60ae11b60a4
tareqhajer <tareqhajer@gmail.com>
2021-03-05 23:12:59 +0200

    DateFilter

---
+++ KokazGoodsTransfer/Dtos/OrdersDtos/DateFiter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KokazGoodsTransfer.Dtos.OrdersDtos
+{
+    public class DateFiter
+    {
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+    }
+}
