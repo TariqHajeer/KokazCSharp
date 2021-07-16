939befc3d05d9fb3958977f48f993c87fa482342
= <=>
2021-07-16 10:31:54 +0300

    OrderLogDto

--- KokazGoodsTransfer/Dtos/OrdersDtos/OrderLogDto.cs
+++ KokazGoodsTransfer/Dtos/OrdersDtos/OrderLogDto.cs
@@ -10,40 +10,43 @@ using System.Threading.Tasks;
 
 namespace KokazGoodsTransfer.Dtos.OrdersDtos
 {
-    public class OrderDto
+    public class OrderLogDto
     {
-
         public int Id { get; set; }
         public string Code { get; set; }
+        public int ClientId { get; set; }
+        public int CountryId { get; set; }
         public decimal DeliveryCost { get; set; }
         public decimal Cost { get; set; }
-        public decimal AgentCost { get; set; }
         public decimal? OldCost { get; set; }
+        public decimal AgentCost { get; set; }
         public string RecipientName { get; set; }
         public string RecipientPhones { get; set; }
+        public int? RegionId { get; set; }
         public string Address { get; set; }
         public string ClientNote { get; set; }
-        public string CreatedBy { get; set; }
-        public bool Seen { get; set; }
+        public int MoenyPlacedId { get; set; }
+        public int OrderplacedId { get; set; }
         public DateTime? Date { get; set; }
         public DateTime? DiliveryDate { get; set; }
         public string Note { get; set; }
+        public int? AgentId { get; set; }
+        public bool? Seen { get; set; }
         public bool IsClientDiliverdMoney { get; set; }
-        public int? AgentPrintNumber { get; set; }
-        public int? ClientPrintNumber { get; set; }
+        public bool IsSync { get; set; }
         public int OrderStateId { get; set; }
-        public string CanResned { get; set; }
-        public decimal? OldDeliveryCost { get; set; }
+        public string UpdatedBy { get; set; }
+        public int OrderId { get; set; }
+        public bool? IsDollar { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string SystemNote { get; set; }
+
+        public UserDto Agent { get; set; }
         public ClientDto Client { get; set; }
         public CountryDto Country { get; set; }
         public RegionDto Region { get; set; }
         public NameAndIdDto MonePlaced { get; set; }
         public NameAndIdDto Orderplaced { get; set; }
-        public UserDto Agent { get; set; }
-        public List<ResponseOrderItemDto> OrderItems { get; set; }
-        public List<OrderLogDto> OrderLogDto { get; set; }
-
-
-
+        
     }
 }
