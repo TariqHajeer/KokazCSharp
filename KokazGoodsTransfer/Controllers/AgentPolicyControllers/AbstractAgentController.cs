fd295018a5a68dc02e6f0238398ecfe64e282a1d
= <=>
2021-10-07 16:05:25 +0300

    AgentOrder

---
+++ KokazGoodsTransfer/Controllers/AgentPolicyControllers/AbstractAgentController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using KokazGoodsTransfer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KokazGoodsTransfer.Controllers.AgentPolicyControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Policy = "Agent")]
+    public class AbstractAgentController : AbstractController
+    {
+        public AbstractAgentController(KokazContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+    }
+}
---
+++ KokazGoodsTransfer/Controllers/AgentPolicyControllers/AgentOrder.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using KokazGoodsTransfer.Dtos.OrdersDtos;
+using KokazGoodsTransfer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KokazGoodsTransfer.Controllers.AgentPolicyControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentOrder : AbstractAgentController
+    {
+        public AgentOrder(KokazContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+        [HttpGet("Order")]
+        public IActionResult GetOrder()
+        {
+            var orders = this.Context.Orders.Where(c => c.AgentId == AuthoticateUserId()).ToList();
+            return Ok(mapper.Map<OrderDto[]>(orders));
+        }
+    }
+}
