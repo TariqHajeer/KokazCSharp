5e7298df585c21957fefbfe5ec2a22e4d74699ae
= <=>
2021-10-12 07:35:42 +0300

    print agnet

--- KokazGoodsTransfer/Controllers/AgentPolicyControllers/AgentOrder.cs
+++ KokazGoodsTransfer/Controllers/AgentPolicyControllers/AgentOrder.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using KokazGoodsTransfer.Dtos.AgentDtos;
+using KokazGoodsTransfer.Dtos.Common;
 using KokazGoodsTransfer.Dtos.OrdersDtos;
 using KokazGoodsTransfer.Models;
 using KokazGoodsTransfer.Models.Static;
@@ -28,8 +30,8 @@ namespace KokazGoodsTransfer.Controllers.AgentPolicyControllers
         [HttpGet("InStock")]
         public IActionResult GetInStock()
         {
-           var orders= this.Context.Orders.Where(c => c.OrderplacedId == (int)OrderplacedEnum.Store && c.AgentId == AuthoticateUserId())
-                .ToList();
+            var orders = this.Context.Orders.Where(c => c.OrderplacedId == (int)OrderplacedEnum.Store && c.AgentId == AuthoticateUserId())
+                 .ToList();
             return Ok(mapper.Map<OrderDto[]>(orders));
         }
         [HttpGet("InWay")]
@@ -42,9 +44,26 @@ namespace KokazGoodsTransfer.Controllers.AgentPolicyControllers
         [HttpGet("OrderSuspended")]
         public IActionResult OrderSuspended()
         {
-            var orders = this.Context.Orders.Where(c => (c.OrderplacedId<=(int)OrderplacedEnum.Wa||c.OrderplacedId==(int) OrderplacedEnum.Delayed) && c.AgentId == AuthoticateUserId())
+            var orders = this.Context.Orders.Where(c => c.OrderStateId == (int)OrderStateEnum.Processing && (c.OrderplacedId >= (int)OrderplacedEnum.Way || c.OrderplacedId == (int)OrderplacedEnum.Delayed) && c.AgentId == AuthoticateUserId())
                  .ToList();
             return Ok(mapper.Map<OrderDto[]>(orders));
+        }
+        [HttpGet("Print")]
+        public IActionResult GetPrint([FromQuery] PagingDto pagingDto, PrintFilterDto printFilterDto)
+        {
+            var printeds = this.Context.Printeds.Where(c => c.Type == PrintType.Agent);
+            if (printFilterDto.Date != null)
+            {
+                printeds = printeds.Where(c => c.Date == printFilterDto.Date);
+            }
+            if (printFilterDto.Number != null)
+            {
+                printeds = printeds.Where(c => c.PrintNmber == printFilterDto.Number);
+            }
+            printeds = printeds.Where(c => c.OrderPrints.Any(c => c.Order.AgentId == AuthoticateUserId()));
+            var total = printeds.Count();
+            var list = printeds.Skip(pagingDto.Page - 1).Take(pagingDto.RowCount * pagingDto.Page).ToList();
+            return Ok(new { total, Data = mapper.Map<PrintDto[]>(list) });
         }
     }
 }
---
+++ KokazGoodsTransfer/Dtos/AgentDtos/PrintFilterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KokazGoodsTransfer.Dtos.AgentDtos
+{
+    public class PrintFilterDto
+    {
+        public DateTime? Date { get; set; }
+        public int? Number { get; set; }
+    }
+}
