2175df58f58ba1ffb8fc644093e3be62493cbe61
= <=>
2022-01-01 15:06:04 +0200

    create folder

---
+++ KokazGoodsTransfer/DAL/Infrastructure/Concret/Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KokazGoodsTransfer.DAL.Infrastructure.Interfaces;
+
+namespace KokazGoodsTransfer.DAL.Infrastructure.Concret
+{
+    public class Repository<T> 
+    {
+    }
+}
---
+++ KokazGoodsTransfer/DAL/Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KokazGoodsTransfer.DAL.Infrastructure.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task Add(T entity);
+        Task<List<T>> GetAll();
+
+    }
+}
