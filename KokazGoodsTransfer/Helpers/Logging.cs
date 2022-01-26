370ee14417559e5c52d7fa729585590028f45967
= <=>
2022-01-27 00:42:07 +0200

    create class

---
+++ KokazGoodsTransfer/Helpers/Logging.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KokazGoodsTransfer.Helpers
+{
+    public class Logging
+    {
+        public async Task WriteExption(Exception exception)
+        { 
+            
+        }
+    }
+}
