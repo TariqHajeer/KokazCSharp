fe4bdaeca18fa07a2dd7c424878dd12cfb9a912a
= <=>
2022-02-09 00:41:29 +0200

    init agent Serivce

---
+++ KokazGoodsTransfer/Services/Concret/AgentSerivce.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using KokazGoodsTransfer.DAL.Infrastructure.Interfaces;
+using KokazGoodsTransfer.Dtos.Users;
+using KokazGoodsTransfer.Models;
+using KokazGoodsTransfer.Services.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KokazGoodsTransfer.Services.Concret
+{
+    public class AgentSerivce : CashService<User, UserDto, CreateUserDto, UpdateUserDto>, IAgentService
+    {
+        public AgentSerivce(IRepository<User> repository, IMapper mapper, IMemoryCache cache) : base(repository, mapper, cache)
+        {
+        }
+        public override async Task<IEnumerable<UserDto>> GetCashed()
+        {
+            var name = typeof(User).FullName;
+            if (!_cache.TryGetValue(name, out IEnumerable<UserDto> entites))
+            {
+                var list = await GetAsync(null, c => c.AgentCountrs.Select(c=>c.Country),c=>c.UserPhones);
+                entites = _mapper.Map<UserDto[]>(list);
+                _cache.Set(name, entites);
+            }
+            return entites;
+        }
+
+    }
+}
---
+++ KokazGoodsTransfer/Services/Interfaces/IAgentService.cs
@@ -0,0 +1,13 @@
+﻿using KokazGoodsTransfer.Dtos.Users;
+using KokazGoodsTransfer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KokazGoodsTransfer.Services.Interfaces
+{
+    public interface IAgentService: ICashService<User, UserDto, CreateUserDto, UpdateUserDto>
+    {
+    }
+}
