1c5393d5632e91de5f525fbe161b0926fe46c3b9
tariqHajeer <tareqhajer@gmail.com>
2022-08-11 00:34:00 +0300

    fix ClientBalance

--- KokazGoodsTransfer/Startup.cs
+++ KokazGoodsTransfer/Startup.cs
@@ -39,8 +39,8 @@ namespace KokazGoodsTransfer
         {
 
             services.AddControllers();
-            services.AddDbContext<KokazContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SmartAsp")));
-            //services.AddDbContext<KokazContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Local")));
+            //services.AddDbContext<KokazContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SmartAsp")));
+            services.AddDbContext<KokazContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Local")));
             //services.AddDbContext<KokazContext>(options => options.UseSqlServer(Configuration.GetConnectionString("goldenWingsDB")));
 
             services.AddCors(options =>
