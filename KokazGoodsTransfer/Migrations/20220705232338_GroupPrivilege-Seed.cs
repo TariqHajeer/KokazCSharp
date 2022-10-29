f962bdcb7df0dddabc0e6c28cc813d4c0f140865
tariqHajeer <tareqhajer@gmail.com>
2022-10-29 13:26:16 +0200

    group privileges

---
+++ KokazGoodsTransfer/Migrations/20220705232338_GroupPrivilege-Seed.cs
@@ -0,0 +1,405 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KokazGoodsTransfer.Migrations
+{
+    public partial class GroupPrivilegeSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "GroupPrivilege",
+                columns: new[] { "GroupId", "PrivilegId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 34 },
+                    { 1, 35 },
+                    { 1, 36 },
+                    { 1, 37 },
+                    { 1, 38 },
+                    { 1, 39 },
+                    { 1, 40 },
+                    { 1, 41 },
+                    { 1, 42 },
+                    { 1, 43 },
+                    { 1, 44 },
+                    { 1, 45 },
+                    { 1, 46 },
+                    { 1, 33 },
+                    { 1, 47 },
+                    { 1, 49 },
+                    { 1, 50 },
+                    { 1, 51 },
+                    { 1, 52 },
+                    { 1, 53 },
+                    { 1, 54 },
+                    { 1, 55 },
+                    { 1, 56 },
+                    { 1, 57 },
+                    { 1, 58 },
+                    { 1, 59 },
+                    { 1, 60 },
+                    { 1, 61 },
+                    { 1, 48 },
+                    { 1, 62 },
+                    { 1, 32 },
+                    { 1, 30 },
+                    { 1, 2 },
+                    { 1, 3 },
+                    { 1, 4 },
+                    { 1, 5 },
+                    { 1, 6 },
+                    { 1, 7 },
+                    { 1, 8 },
+                    { 1, 9 },
+                    { 1, 10 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "GroupPrivilege",
+                columns: new[] { "GroupId", "PrivilegId" },
+                values: new object[,]
+                {
+                    { 1, 11 },
+                    { 1, 12 },
+                    { 1, 13 },
+                    { 1, 14 },
+                    { 1, 31 },
+                    { 1, 15 },
+                    { 1, 17 },
+                    { 1, 18 },
+                    { 1, 19 },
+                    { 1, 20 },
+                    { 1, 21 },
+                    { 1, 22 },
+                    { 1, 23 },
+                    { 1, 24 },
+                    { 1, 25 },
+                    { 1, 26 },
+                    { 1, 27 },
+                    { 1, 28 },
+                    { 1, 29 },
+                    { 1, 16 },
+                    { 1, 63 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 9 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 10 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 11 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 12 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 13 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 14 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 15 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 16 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 17 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 18 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 19 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 20 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 21 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 22 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 23 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 24 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 25 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 26 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 27 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 28 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 29 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 30 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 31 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 32 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 33 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 34 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 35 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 36 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 37 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 38 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 39 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 40 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 41 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 42 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 43 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 44 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 45 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 46 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 47 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 48 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 49 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 50 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 51 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 52 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 53 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 54 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 55 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 56 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 57 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 58 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 59 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 60 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 61 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 62 });
+
+            migrationBuilder.DeleteData(
+                table: "GroupPrivilege",
+                keyColumns: new[] { "GroupId", "PrivilegId" },
+                keyValues: new object[] { 1, 63 });
+        }
+    }
+}
