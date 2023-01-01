4842dce196f3a26411385c0847689d1bce77758c
tariqHajeer <tareqhajer@gmail.com>
2023-01-01 19:59:19 +0200

    include

--- KokazGoodsTransfer/Services/Concret/OrderService.cs
+++ KokazGoodsTransfer/Services/Concret/OrderService.cs
@@ -1954,7 +1954,7 @@ namespace KokazGoodsTransfer.Services.Concret
         }
         public async Task<PagingResualt<IEnumerable<OrderDto>>> GetDisApprovedOrdersReturnedByBranch(PagingDto pagingDto)
         {
-            var orders = await _repository.GetAsync(pagingDto, c => c.IsReturnedByBranch == true && c.CurrentBranchId == _currentBranchId);
+            var orders = await _repository.GetAsync(pagingDto, c => c.IsReturnedByBranch == true && c.CurrentBranchId == _currentBranchId,c=>c.Country,c=>c.Client);
             return new PagingResualt<IEnumerable<OrderDto>>()
             {
                 Total = orders.Total,
