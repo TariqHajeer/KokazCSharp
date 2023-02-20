cd18b43ce06dc84707a8cbf2ef9b32502c50c6b6
tariqHajeer <tareqhajer@gmail.com>
2023-02-20 20:45:52 +0200

    RequiredAgent

--- KokazGoodsTransfer/Controllers/EmployeePolicyControllers/CountryController.cs
+++ KokazGoodsTransfer/Controllers/EmployeePolicyControllers/CountryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using KokazGoodsTransfer.DAL.Infrastructure.Interfaces;
 using KokazGoodsTransfer.Dtos.Countries;
 using KokazGoodsTransfer.Helpers;
 using KokazGoodsTransfer.Models;
@@ -21,18 +22,33 @@ namespace KokazGoodsTransfer.Controllers.EmployeePolicyControllers
         private readonly IRegionCashedService _regionCashedService;
         private readonly IUserCashedService _userCashedService;
         private readonly IClientCashedService _clientCashedService;
-        public CountryController(ICountryCashedService countryCashedService, IRegionCashedService regionCashedService, IUserCashedService userCashedService, IClientCashedService clientCashedService)
+        private readonly IRepository<MediatorCountry> _mediatorCountry;
+        private readonly IHttpContextAccessorService _httpContextAccessorService;
+        private readonly IRepository<Branch> _branchRepository;
+        private readonly int _currentBranchId;
+        public CountryController(ICountryCashedService countryCashedService, IRegionCashedService regionCashedService, IUserCashedService userCashedService, IClientCashedService clientCashedService, IRepository<MediatorCountry> mediatorCountry, IHttpContextAccessorService httpContextAccessorService, IRepository<Branch> branchRepository)
         {
             _countryCashedService = countryCashedService;
             _regionCashedService = regionCashedService;
             _userCashedService = userCashedService;
             _clientCashedService = clientCashedService;
+            _mediatorCountry = mediatorCountry;
+            _httpContextAccessorService = httpContextAccessorService;
+            _currentBranchId = _httpContextAccessorService.CurrentBranchId();
+            _branchRepository = branchRepository;
         }
         private void RemoveRelatedCash()
         {
             _regionCashedService.RemoveCash();
             _userCashedService.RemoveCash();
             _clientCashedService.RemoveCash();
+        }
+        [HttpGet("RequiredAgent")]
+        public async Task<ActionResult<bool>> RequiredAgent(int countryId)
+        {
+            var currentBranch = await _branchRepository.GetById(_currentBranchId);
+            return Ok(!(await _mediatorCountry.Any(c => c.FromCountryId == currentBranch.CountryId && c.ToCountryId == countryId)));
+
         }
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CountryDto>>> GetAll() => Ok(await _countryCashedService.GetCashed());
