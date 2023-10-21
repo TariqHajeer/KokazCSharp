ad8e3c173b5876fdd6a1306559e4cd9f78a7262a
tariqHajeer <tareqhajer@gmail.com>
2023-10-21 16:05:25 +0300

    add branch information

---
+++ Quqaz.Web/Controllers/ClientPolicyControllers/CBranchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quqaz.Web.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace Quqaz.Web.Controllers.ClientPolicyControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CBranchController : ControllerBase
+    {
+        private readonly IBranchService _branchService;
+        public CBranchController(IBranchService branchService)
+        {
+            _branchService = branchService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetALL()
+        {
+
+            return Ok(await _branchService.GetAll());
+
+        }
+    }
+}
--- Quqaz.Web/Models/Branch.cs
+++ Quqaz.Web/Models/Branch.cs
@@ -10,6 +10,8 @@ namespace Quqaz.Web.Models
         public string Name { get; set; }
         [ForeignKey(nameof(Id))]
         public virtual Country Country { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
         public virtual ICollection<BranchToCountryDeliverryCost> BranchToCountryDeliverryCosts { get; set; }
     }
 }
